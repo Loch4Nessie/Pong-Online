388abc19c975d58de01b121657265872c6617eb3
Loch4Nessie <45662690+Loch4Nessie@users.noreply.github.com>
2019-11-20 17:20:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 
 var express = require('express');
-
 var app = express();
 var server = app.listen(4000); //Zorgt er voor dat de server op port 4000 gaat runnen als hij aan staat
 
 app.use(express.static('public')); //runt elke file die in de public directory staan
 
-console.log("The server is running!");
+console.log("Server is running");
 
 var socket = require('socket.io');
-
 var io = socket(server);
 
 io.on('connection', function(socket){
 	console.log('new connection: ' + socket.id);
 
-	socket.on('player2Data', function(player2Data){
-		socket.broadcast.emit('player2Data', player2Data);
-	})
+	socket.on('p1yPos', function(p1yData){
+		
+		socket.broadcast.emit('p1yPos', p1yData);
+	});
 });
