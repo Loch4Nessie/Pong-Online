d903364a81ace96505778c0b8496116c2556bee0
Loch4Nessie <45662690+Loch4Nessie@users.noreply.github.com>
2019-11-20 17:21:35 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,64 +1,109 @@
-let socket;
+let socket = io();
 let xSize = 1280;
 let ySize = 720;
-
 let Player1;
+let Player2;
+let Ball;
+
+let initGame = true;
+let startGame = false;
+
 
 function setup() {
-	createCanvas(xSize, ySize);	
-//	socket = io.connect();
-    socket.on('player2Data', player2);
-}
-
-function player2(player2Data) {
-  noStroke();
-  fill("blue");
-  rect(1230, player2Data.y, 20, 60);
+	createCanvas(xSize, ySize);
+	socket = io.connect();
 
 }
 
 function draw() {
-  background("black");
-  
-  Player1 = new player1();
-  Player1.display();
-  Player1.move();
-  
+	background(1, 100, );
+
+	if(initGame == true) {
+		Player1 = new player1();
+		Ball = new ball();
+
+		initGame = false;
+		startGame = true;
+	} 
+	else if (startGame == true) {
+	
+		Player1.display();
+		Player1.move();
+
+		Ball.display();
+		Ball.move();
+
+		let p1yData = {
+			y: Player1.yPos
+		}
+		socket.emit('p1yPos', p1yData)
+
+		socket.on('p1yPos', function(p1yData){
+			noStroke();
+			fill("white");
+			rect(xSize-120, p1yData.y, 20, 140);
+		});
+
+	}
 }
+
 
 class player1 {
 	constructor() {
-		this.xPos = 50;
-		this.yPos = 100;
+		this.xPos = 100;
+		this.yPos = ySize/2 - 76;
 		this.width = 20;
-		this.height = 60;
+		this.height = 140;
 		this.speed = 10;
 	}
 
 	display() {
 		noStroke();
-		fill("red");
+		fill("white");
 		rect(this.xPos, this.yPos, this.width, this.height);
 	}
 
 	move() {
-		if(keyIsDown(38) && this.yPos > 0) {
+		if(keyIsDown(87) && this.yPos > 0) {
 			this.yPos -= this.speed;
 		}
 
-		if(keyIsDown(40) && this.yPos < ySize) {
+		if(keyIsDown(83) && this.yPos < ySize - this.height) {
 			this.yPos += this.speed;
 		}
 	}
+
 }
 
-let player2Data = {
-  y: player1.yPos
+
+class ball {
+	constructor() {
+		this.xPos = xSize/2;
+		this.yPos = ySize/2;
+		this.radius = 20;
+		this.xSpeed = 10;
+		this.ySpeed = 10;
+	}
+
+	display() {
+		noStroke();
+		fill("white");
+		ellipse(this.xPos, this.yPos, this.radius, this.radius);
+	}
+
+	move() {
+   		if (this.xPos > width - this.radius || this.xPos < this.radius) {
+      		this.xSpeed = -this.xSpeed;
+    	}
+
+    	if (this.yPos > height - this.radius || this.yPos < this.radius) {
+      		this.ySpeed = -this.ySpeed;
+    	}
+
+   		 this.xPos += this.xSpeed;
+    	 this.yPos += this.ySpeed;
+  	}
 }
-socket.emit('player2Data', player2Data);
-
-
-
 
 
 
